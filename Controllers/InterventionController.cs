0b4814dade9ded8718b3766b7fbd3068931a5293
francis renauld <renauldf12@gmail.com>
2022-07-28 14:24:33 -0400

    add finish project

---
+++ Controllers/InterventionController.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RocketElevators.Models;
+
+namespace RocketElevators.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterventionController : ControllerBase
+    {
+        private readonly RocketElevatorsContext _context;
+
+        public InterventionController(RocketElevatorsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Intervention
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Intervention>>> Getinterventions()
+        {
+          if (_context.interventions == null)
+          {
+              return NotFound();
+          }
+            return await _context.interventions.ToListAsync();
+        }
+
+         // GET: /api/InterventionPending
+         [HttpGet("/api/InterventionPending")]
+        public async Task<ActionResult<IEnumerable<Intervention>>> GetInterventionStatusPending()
+        {
+          if (_context.interventions == null)
+          {
+              return NotFound();
+          }
+            return await _context.interventions.Where(intervention => intervention.status =="Pending" && intervention.start_date_and_time_of_the_intervention == null).ToListAsync();
+        }
+
+        // GET: api/Intervention/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Intervention>> GetIntervention(long id)
+        {
+          if (_context.interventions == null)
+          {
+              return NotFound();
+          }
+            var intervention = await _context.interventions.FindAsync(id);
+
+            if (intervention == null)
+            {
+                return NotFound();
+            }
+
+            return intervention;
+        }
+
+        // PUT: api/Intervention/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIntervention(long id)
+        {
+             if (_context.interventions == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InterventionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // PUT: api/Intervention/5/StartIntervention
+        [HttpPut("{id}/StartIntervention")]
+        public async Task<IActionResult> StartIntervention(long id)
+        {
+            var interventionID = _context.interventions.FirstOrDefault(intervention => intervention.Id == id);
+
+            if (_context.interventions == null)
+            {
+                return BadRequest();
+            }
+
+            if(interventionID != null)
+            {
+                interventionID.status = "InProgress";
+                interventionID.start_date_and_time_of_the_intervention = DateTime.Now;
+
+            }
+        
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InterventionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(interventionID);
+        }
+
+        // PUT: api/Intervention/5/EndIntervention
+        [HttpPut("{id}/EndIntervention")]
+        public async Task<IActionResult> EndIntervention(long id)
+        {
+            var interventionID = _context.interventions.FirstOrDefault(intervention => intervention.Id == id);
+
+            if (_context.interventions == null)
+            {
+                return BadRequest();
+            }
+
+            if(interventionID != null)
+            {
+                interventionID.status = "Completed";
+                interventionID.end_date_and_time_of_the_intervention = DateTime.Now;
+
+            }
+        
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InterventionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(interventionID);
+        }
+
+        // POST: api/Intervention
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Intervention>> PostIntervention(Intervention intervention)
+        {
+          if (_context.interventions == null)
+          {
+              return Problem("Entity set 'RocketElevatorsContext.interventions'  is null.");
+          }
+            _context.interventions.Add(intervention);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetIntervention", new { id = intervention.Id }, intervention);
+        }
+
+        // DELETE: api/Intervention/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIntervention(long id)
+        {
+            if (_context.interventions == null)
+            {
+                return NotFound();
+            }
+            var intervention = await _context.interventions.FindAsync(id);
+            if (intervention == null)
+            {
+                return NotFound();
+            }
+
+            _context.interventions.Remove(intervention);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool InterventionExists(long id)
+        {
+            return (_context.interventions?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Rest-Api
+# Context WEEK 8
+
+For this week's project, we had to create 2 APIs along with a new table for our postgreSQL database and fake data for the new table. Our first API was made using graphql on Rails and our second API is a REST API with C#. The goal of this week's project was to allow us to get to know a lot more about APIs and how they work. We used ruby on rails, c# for our programing languages along with heroku to host our APIs online and postman to do GET,PUT,POST requests to test our REST API and publish our work for both APIs.
+
+Link to our Website => https://rocketelevators.cfd/
+
+Link to our Video => https://youtu.be/tKcguiwSwcM
+
+## The following example shows a REST API request with Postman:
+
+### We start by doing a GET to see the status of the battery 5
+
+<img width="1292" alt="image" src="https://user-images.githubusercontent.com/105597570/180489833-b234af98-625d-460c-b889-203ac64efa53.png">
+
+### We then do a PUT to change the status of the battery 5 from "Intervention" to "Active"
+
+<img width="1288" alt="image" src="https://user-images.githubusercontent.com/105597570/180490555-c89cc74e-c4dd-4565-9e48-39dee800fb96.png">
+
+### As you can see, when we do a GET again, the status of the battery 5 changed and is now "Active" 
+
+<img width="1286" alt="image" src="https://user-images.githubusercontent.com/105597570/180490669-85ffcbcc-c926-4675-9757-39847feb04e1.png">
+
+# JSON link for our collection of requests for Postman
+```
+https://www.getpostman.com/collections/69bb0a21411e5a31393b
+```
+---
+
+# Dependencies
+
+  To test our project, you will need the following :
+
+-   Postman => https://www.postman.com/downloads/
