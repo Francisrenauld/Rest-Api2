aa4cc333278d4bc970e4c4c043e29eb07de00046
francis renauld <renauldf12@gmail.com>
2022-08-10 13:31:50 -0400

    Update battery get info

--- Models/Battery.cs
+++ Models/Battery.cs
@@ -4,6 +4,9 @@ namespace RocketElevators.Models
     {
         public long Id { get; set; }
         public string? Status { get; set; }
+        public string? Certificate_Of_Operations { get; set; }
+        public string? Notes { get; set; }
+        public DateTime? Date_Of_Commissioning { get; set; }
         public long building_id { get; set; }
         public Building? Building { get; set; }
         public List<Column>? Columns { get; set; }
