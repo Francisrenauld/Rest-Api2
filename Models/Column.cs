450d0e6cd3db5d12d4f8195f586f41b919c303fe
francis renauld <renauldf12@gmail.com>
2022-08-10 13:48:35 -0400

    Update olumn get info

--- Models/Column.cs
+++ Models/Column.cs
@@ -4,6 +4,7 @@ namespace RocketElevators.Models
     {
         public long Id { get; set; }
         public string? Status { get; set; }
+        public string? Notes { get; set; }
         public long battery_id { get; set; }
         public Battery? Batteries { get; set; }
         public List<Elevator>? Elevators { get; set; }
