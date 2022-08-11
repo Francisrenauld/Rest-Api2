a282bc6cd21610462c434042efc13d8c49f5018e
francis renauld <renauldf12@gmail.com>
2022-08-11 15:02:43 -0400

    ddffdsf

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using RocketElevators.Models;
-
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddControllers();
 // builder.Services.AddDbContext<RocketElevatorsContext>(opt =>
     // opt.UseInMemoryDatabase("TodoList"));
-builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
-            {
-            builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-            }));
+builder.Services.AddCors(o => o.AddPolicy(MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("https://localhost:7114/")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                      }));
 builder.Services.AddDbContext<RocketElevatorsContext>(options => {
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
@@ -26,20 +26,15 @@ builder.Services.AddDbContext<RocketElevatorsContext>(options => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-app.UseCors(options => options.AllowAnyOrigin());  
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
