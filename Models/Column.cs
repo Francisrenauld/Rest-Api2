2fb47d340f0b08fbe3f284b598898513fdb55f28
francis renauld <renauldf12@gmail.com>
2022-08-12 11:04:23 -0400

    Add type to get column by battery

--- Models/Column.cs
+++ Models/Column.cs
@@ -5,6 +5,7 @@ namespace RocketElevators.Models
         public long Id { get; set; }
         public string? Status { get; set; }
         public string? Notes { get; set; }
+        public string? Type { get; set; }
         public long battery_id { get; set; }
         public Battery? Batteries { get; set; }
         public List<Elevator>? Elevators { get; set; }
